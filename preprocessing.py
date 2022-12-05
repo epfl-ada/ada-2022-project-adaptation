9218947e2164be6c11e2884130e37e32910b4074
wenxiudu <71752563+wenxiudu@users.noreply.github.com>
2022-12-05 16:55:50 +0100

    initialising MS3

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,6 @@ dfs = []
 
 for df_json in pd.read_json(PATH_METADATA_DST, compression="infer", chunksize=10000, lines=True):
     df_json.drop(["channel_id", "categories", "dislike_count", "duration", "like_count", "description", "upload_date", "view_count", "title", "crawl_date"], inplace=True, axis=1)
-    df_json["tags"] = df_json["tags"].apply(lambda x : x.split(","))
     dfs.append(df_json)
 
 df_final = pd.concat(dfs)
