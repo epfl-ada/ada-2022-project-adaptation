e125b12efb63ef5da833c73f1a5938e391c49134
wenxiudu <71752563+wenxiudu@users.noreply.github.com>
2022-12-06 19:54:15 +0100

    sentiment analysis started

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,7 @@ PATH_METADATA_TAGS_HELPER = DIR + "yt_metadata_tags_helper.feather"
 dfs = []
 
 for df_json in pd.read_json(PATH_METADATA_DST, compression="infer", chunksize=10000, lines=True):
-    df_json.drop(["channel_id", "categories", "dislike_count", "duration", "like_count", "description", "upload_date", "view_count", "title", "crawl_date"], inplace=True, axis=1)
+    df_json.drop(["channel_id", "dislike_count", "duration", "like_count", "description", "upload_date", "view_count", "title", "crawl_date"], inplace=True, axis=1)
     dfs.append(df_json)
 
 df_final = pd.concat(dfs)
