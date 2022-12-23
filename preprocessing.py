abfcc9b3b6bf3650db7e2148cc5485a9f8e7f1fa
Paul Nadal <paul.nadal@epfl.ch>
2022-12-23 20:29:27 +0100

    compiled notebook

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,15 @@ PATH_METADATA_TAGS_HELPER = DIR + "yt_metadata_tags_helper.feather"
 dfs = []
 
 for df_json in pd.read_json(PATH_METADATA_DST, compression="infer", chunksize=10000, lines=True):
+    df_json.drop(["channel_id", "dislike_count", "duration", "like_count", "description", "upload_date", "tags", "view_count", "crawl_date"], inplace=True, axis=1)
+    dfs.append(df_json)
+
+df_final = pd.concat(dfs)
+df_final.to_feather(PATH_METADATA_TITLE_HELPER)
+
+dfs = []
+
+for df_json in pd.read_json(PATH_METADATA_DST, compression="infer", chunksize=10000, lines=True):
     df_json.drop(["channel_id", "dislike_count", "duration", "like_count", "description", "upload_date", "view_count", "title", "crawl_date"], inplace=True, axis=1)
     dfs.append(df_json)
 
